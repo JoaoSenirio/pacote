22f2a4fd572b6494c00c1694a200da3dfe6c62fe
João Senírio de Sousa Costa <senirio10@gmail.com>
2021-05-04 22:03:21 -0300

    Corrigindo o modo de indicação da alteração feita

--- micronucleus_source-code/library/micronucleus_lib.h
+++ micronucleus_source-code/library/micronucleus_lib.h
@@ -6,8 +6,6 @@
   (c) 2012 by ihsan Kehribar <ihsan@kehribar.me>
   Changes for Micronucleus protocol version V2.x
   (c) 2014 T. Bo"scke
-  (c) 2020 Franzininho 
-  Translated output mensages to Portuguese
 
   Permission is hereby granted, free of charge, to any person obtaining a copy of
   this software and associated documentation files (the "Software"), to deal in
@@ -26,6 +24,8 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.
+
+  Modified in 2021 by Franzininho
 */
 
 /********************************************************************************
